cdf89dbed4e02f08f521ee36cffd34a1445a7508
Gustavo Barragan <dev@droni.co>
2024-03-08 03:58:15 -0500

    redirect on english version

--- server/middleware/redirect.ts
+++ server/middleware/redirect.ts
@@ -3,4 +3,7 @@ export default defineEventHandler(async (event) => {
   if(urlObj.pathname.startsWith('/es/posts/')) {
     await sendRedirect(event, urlObj.pathname.replace("/es/posts/", '/es/blog/2024/'), 301)
   }
+  if(urlObj.pathname.startsWith('/en/posts/')) {
+    await sendRedirect(event, urlObj.pathname.replace("/en/posts/", '/en/blog/2024/'), 301)
+  }
 })
