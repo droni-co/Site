91469f9fc77596152faaa8bc5f48f861e59b0d34
Gustavo Barragan <dev@droni.co>
2024-06-17 23:49:08 -0500

    live screen

---
+++ pages/live.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex h-screen">
+    <div class="grow">
+      <iframe
+        class="w-full aspect-video"
+        title="dronico"
+        src="https://player.twitch.tv/?channel=dronico&parent=droni.co"
+        allowfullscreen="true"></iframe>
+    </div>
+    <div>
+      <iframe
+        class="w-full h-full"
+        title="chat"
+        id="chat_embed"
+        src="https://www.twitch.tv/embed/dronico/chat?parent=droni.co"">
+      </iframe>
+    </div>
+  </div>
+</template>
