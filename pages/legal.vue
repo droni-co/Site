08441afdf0c6cc305081df940d6143f9b7e46d35
Gustavo Barragan <dev@droni.co>
2024-06-19 03:16:21 -0500

    legal page

---
+++ pages/legal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <UiHero>
+      <div class="text-center md:text-start py-6">
+        <h1 class="text-balance lg:text-4xl text-gray-800 drop-shadow-lg dark:text-gray-50">
+          {{ doc?.title }}
+        </h1>
+        <h4>{{ doc?.subtitle }}</h4>
+      </div>
+    </UiHero>
+    <div class="container mx-auto px-2 md:px-auto">
+      <article class="prose lg:prose-xl max-w-full dark:prose-invert md:my-8">
+        <ContentDoc />
+      </article>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const localePath = useLocalePath()
+const route = useRoute()
+const { data:doc } = await useAsyncData('page-data', () => queryContent(route.fullPath).findOne())
+</script>
