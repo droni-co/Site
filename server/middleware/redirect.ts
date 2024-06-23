adeaca690625bafa543fae0a3f3f7cfc97646119
Gustavo Barragan <dev@droni.co>
2024-06-23 02:38:43 -0500

    redirect www 301

--- server/middleware/redirect.ts
+++ server/middleware/redirect.ts
@@ -1,9 +1,15 @@
 export default defineEventHandler(async (event) => {
   const urlObj = getRequestURL(event)
+  const host = event.node.req.headers.host
   if(urlObj.pathname.startsWith('/es/posts/')) {
-    await sendRedirect(event, urlObj.pathname.replace("/es/posts/", '/es/blog/2024/'), 301)
+    await sendRedirect(event, urlObj.pathname.replace("/es/posts/", '/es/blog/'), 301)
   }
   if(urlObj.pathname.startsWith('/en/posts/')) {
-    await sendRedirect(event, urlObj.pathname.replace("/en/posts/", '/es/blog/2024/'), 301)
+    await sendRedirect(event, urlObj.pathname.replace("/en/posts/", '/es/blog/'), 301)
+  }
+
+  // remove www
+  if (host?.startsWith('www.')) {
+    await sendRedirect(event, `https://${host.replace('www.', '')}${urlObj.pathname}${urlObj.search}`, 301)
   }
 })
